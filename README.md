d7b78ac84e1f43c6d25b470c684bebc69f1f5b0b
Toby <liuning0820@outlook.com>
2018-11-15 10:57:57 +0800

    ajust the folder structure.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# acs-bat-shell
-acs working style (a, means automatic) related bat, bash, shell to effective my daily work.
+# acs-bat-bash-shell
+
+ACS working style (a, means automatic) related bat, bash, shell to effective my daily work.
 
 https://www.windows-commandline.com/category/files/
--- sendentary-reminder.ps1
+++
@@ -1,6 +0,0 @@
-﻿[reflection.assembly]::loadwithpartialname("System.Windows.Forms")
-[reflection.assembly]::loadwithpartialname("System.Drawing")
-$notify = new-object system.windows.forms.notifyicon
-$notify.icon = [System.Drawing.SystemIcons]::Information
-$notify.visible = $true
-$notify.showballoontip(10,"健康贴士!","休息一h喝杯水吧!",[system.windows.forms.tooltipicon]::None) 
