65743cfa7240cef509b012c2b06aeb29e51a8e0b
liuning0820 <liuning0820@outlook.com>
2019-11-09 11:29:51 +0800

    Fix issue found by checkshell

--- src/bash/function.sh
+++ src/bash/function.sh
@@ -2,10 +2,10 @@
 
 function ipv4_addr()
 {
-    IP_VAR=`ifconfig eth0 | grep "inet " | tr -s " " ":"|cut -f3 -d ":"`
-    echo "Host IPv4 address:" $IP_VAR
+    IP_VAR=$(ifconfig eth0 | grep "inet " | tr -s " " ":"|cut -f3 -d ":")
+    echo "Host IPv4 address:" "$IP_VAR"
 }
 
-echo "Computer name:" $HOSTNAME
-echo "Currently logged in user:" $USER
+echo "Computer name:" "$HOSTNAME"
+echo "Currently logged in user:" "$USER"
 ipv4_addr
